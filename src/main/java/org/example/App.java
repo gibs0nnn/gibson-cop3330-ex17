26e3d192f5b6d41cf6b641f12f07d932eebc0e39
gibs0nnn <gibs0n@knights.ucf.edu>
2021-09-11 11:30:57 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,43 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 17 Solution
+ *  Copyright 2021 Nathaniel Gibson
+ */
+
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a 1 if you are male or a 2 if you are a female: ");
+        int gender = scanner.nextInt();
+        
+        double distRatio = 0;
+        if(gender == 1) {
+            distRatio = 0.73;
+        } else if (gender == 2) {
+            distRatio = 0.66;
+        } else {
+            System.out.println("Incorrect input given");
+        }
+        System.out.print("How many ounces of alcohol did you have? ");
+        int alc = scanner.nextInt();
+        System.out.print("What is your weight, in pounds? ");
+        int weight = scanner.nextInt();
+        System.out.print("How many hours has it been since your last drink? ");
+        int hours = scanner.nextInt();
+
+        double bloodAlc = (alc * 5.14 / weight * distRatio) - .015 * hours;
+        System.out.println("\nYour BAC is " + Math.round(bloodAlc * 1000000.0) / 1000000.0);
+        if(bloodAlc > 0.08) {
+            System.out.println("It is not legal for you to drive");
+        } else {
+            System.out.println("It is legal for you to drive");
+        }
+
+    }
+}
