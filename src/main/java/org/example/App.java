9efa35446c8b95ffd0ceffe6ec119f323a6fd8ca
gibs0nnn <gibs0n@knights.ucf.edu>
2021-09-13 20:35:42 -0400

    Organized and Commented Code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,17 +13,22 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+
+        //Gender Input
         System.out.print("Enter a 1 if you are male or a 2 if you are a female: ");
         int gender = scanner.nextInt();
-        
+
+        //Gender Sort
         double distRatio = 0;
         if(gender == 1) {
             distRatio = 0.73;
         } else if (gender == 2) {
             distRatio = 0.66;
         } else {
-            System.out.println("Incorrect input given");
+            System.out.println("Incorrect input given.");
         }
+
+        //Alcohol Consumption and Weight Inputs
         System.out.print("How many ounces of alcohol did you have? ");
         int alc = scanner.nextInt();
         System.out.print("What is your weight, in pounds? ");
@@ -31,12 +36,15 @@ public class App
         System.out.print("How many hours has it been since your last drink? ");
         int hours = scanner.nextInt();
 
+        //Calculation
         double bloodAlc = (alc * 5.14 / weight * distRatio) - .015 * hours;
+
+        //Output
         System.out.println("\nYour BAC is " + Math.round(bloodAlc * 1000000.0) / 1000000.0);
         if(bloodAlc > 0.08) {
-            System.out.println("It is not legal for you to drive");
+            System.out.println("It is not legal for you to drive.");
         } else {
-            System.out.println("It is legal for you to drive");
+            System.out.println("It is legal for you to drive.");
         }
 
     }
